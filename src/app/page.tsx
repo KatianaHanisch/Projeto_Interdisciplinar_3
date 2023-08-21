6f855e2944560817c10a250d5f30b98929cf56d6
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-08-21 16:09:56 -0400

    fix: Ajustes na pagina web

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import BannerHome from "./(web)/components/BannerHome";
+import HeaderHome from "./(web)/components/HeaderHome";
+
+export default function Home() {
+  return (
+    <main>
+      <HeaderHome />
+      <BannerHome />
+    </main>
+  );
+}
