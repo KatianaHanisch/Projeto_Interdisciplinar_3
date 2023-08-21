03783728e91c819fd369f081d1e12d54c9efa883
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-08-21 16:45:19 -0400

    fix: Ajuste responsividade do banner

--- src/app/(web)/components/BannerHome.tsx
+++ src/app/(web)/components/BannerHome.tsx
@@ -4,9 +4,26 @@ import { BsBook } from "react-icons/bs";
 export default function BannerHome() {
   return (
     <section className="text-gray-900">
-      <Image src="/banner.jpg" alt="exemplo" width={5000} height={600}></Image>
-      <div className="bg-[#EBEBEB] h-[80px] flex items-center justify-center">
-        <div className="flex justify-between w-[1200px] text-xl">
+      <div className="hidden md:block">
+        <Image
+          src="/banner.jpg"
+          alt="exemplo"
+          width={5000}
+          height={600}
+        ></Image>
+      </div>
+
+      <div className="block md:hidden">
+        <Image
+          src="/banner-mobile.jpg"
+          alt="exemplo"
+          width={5000}
+          height={300}
+        ></Image>
+      </div>
+
+      <div className="hidden md:flex bg-[#EBEBEB] h-[80px] items-center justify-center px-6">
+        <div className="flex justify-between w-[1200px] text-base lg:text-xl">
           <div className="flex flex-col items-center">
             <p className="z-10">Mais de tantos livros disponíveis</p>
             <span className="absolute bg-[#EBEBEB] mt-3 p-6 rounded-full text-4xl">
