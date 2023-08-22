c9c54f66f017d2679d8127ec0d4cab3894ca96aa
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-08-22 17:19:59 -0400

    feat: Adicionado componente de input

---
+++ src/app/(login)/login/page.tsx
@@ -0,0 +1,12 @@
+import Input from "../../(web)/components/Input";
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <Input />
+      </form>
+    </div>
+  );
+}
