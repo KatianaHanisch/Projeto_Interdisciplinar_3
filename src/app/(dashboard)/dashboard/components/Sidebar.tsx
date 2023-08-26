86d38f2d951108a740979eb62dbed0e983a93c4b
KatianaHanisch <katianahanisch@gmail.com>
2023-08-26 11:31:33 -0400

    feat: adicionado link active na sidebar

--- src/app/(dashboard)/dashboard/components/Sidebar.tsx
+++ src/app/(dashboard)/dashboard/components/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 import { AiOutlineHome } from "react-icons/ai";
 import { BiBookAdd } from "react-icons/bi";
@@ -14,42 +15,44 @@ import { FaUsers } from "react-icons/fa";
 const routes = [
   {
     label: "Home",
-    icon: AiOutlineHome,
+    Icone: AiOutlineHome,
     href: "/dashboard",
   },
   {
     label: "Adicionar",
-    icon: BiBookAdd,
+    Icone: BiBookAdd,
     href: "/dashboard/adicionar",
   },
   {
     label: "Remover",
-    icon: LuBookX,
+    Icone: LuBookX,
     href: "/dashboard/remover",
   },
   {
     label: "Retiradas",
-    icon: IoBagRemoveOutline,
+    Icone: IoBagRemoveOutline,
     href: "/dashboard/retiradas",
   },
   {
     label: "Pedentes",
-    icon: LuBookUp,
+    Icone: LuBookUp,
     href: "/dashboard/pedentes",
   },
   {
     label: "Finalizados",
-    icon: FaBook,
+    Icone: FaBook,
     href: "/dashboard/finalizados",
   },
   {
     label: "Usuários",
-    icon: FaUsers,
+    Icone: FaUsers,
     href: "/dashboard/usuarios",
   },
 ];
 
 export default function Sidebar() {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-gray-300">
       <div className="px-3 py-2 flex-1">
@@ -65,16 +68,26 @@ export default function Sidebar() {
           </h1>
         </Link>
         <div className="flex flex-col h-3/4  justify-around">
-          {routes.map((route) => (
-            <Link key={route.href} href={route.href}>
-              <div className="flex items-center p-2 hover:bg-gray-100 transition rounded">
-                <route.icon className="h-7 w-7 mr-1" color="#171f29" />
-                <h2 className="font-semibold text-lg text-gray-700 ">
-                  {route.label}
-                </h2>
-              </div>
-            </Link>
-          ))}
+          {routes.map(({ href, label, Icone }) => {
+            const isActive = pathname === href;
+
+            return (
+              <Link key={href} href={href}>
+                <div
+                  className={`flex items-center p-2  transition rounded ${
+                    isActive
+                      ? "bg-gray-100 hover:bg-gray-100 shadow-sm"
+                      : "bg-gray-300 hover:bg-gray-200"
+                  }`}
+                >
+                  <Icone className="h-7 w-7 mr-1" color="#171f29" />
+                  <h2 className="font-semibold text-lg text-gray-700 ">
+                    {label}
+                  </h2>
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
