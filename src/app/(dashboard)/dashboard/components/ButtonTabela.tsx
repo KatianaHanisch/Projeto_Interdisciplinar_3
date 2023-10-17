1962eeaf844cad28eff3718f1296652a1aac8cd0
KatianaHanisch <katianahanisch@gmail.com>
2023-10-17 09:56:16 -0400

    feat: criado e implementado  rota para reservas na dashboard

--- src/app/(dashboard)/dashboard/components/ButtonTabela.tsx
+++ src/app/(dashboard)/dashboard/components/ButtonTabela.tsx
@@ -12,7 +12,11 @@ export default function ButtonTabela({
       onClick={abrirModal}
       className={`w-4/6 flex items-center text-white justify-center px-2 py-1  text-right text-base font-medium rounded-md ${
         corButton === "verde" ? "bg-green-900" : "bg-red-900"
-      } ${tipo === "finalizado" ? "cursor-default" : "cursor-pointer"}`}
+      } ${
+        tipo === "finalizado" || tipo === "reserva"
+          ? "cursor-default"
+          : "cursor-pointer"
+      }`}
     >
       {Icone}
       {tituloButton}
---
+++ src/app/api/dashboard/reservasLivros/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/app/utils/Prisma";
+
+export async function GET() {
+  try {
+    const reservas = await prisma.reservas.findMany({
+      include: {
+        livro: true,
+        user: {
+          select: {
+            name: true,
+            phone: true,
+          },
+        },
+      },
+    });
+
+    const livrosReservados = reservas.map((reserva) => ({
+      id: reserva.id,
+      livro: reserva.livro.titulo,
+      usuario: reserva.user.name,
+      telefone: reserva.user.phone,
+    }));
+
+    await prisma.$disconnect;
+
+    return new Response(JSON.stringify(livrosReservados), { status: 200 });
+  } catch (error) {
+    await prisma.$disconnect;
+
+    return new Response("Não foi possível acessar os dados", { status: 500 });
+  }
+}
